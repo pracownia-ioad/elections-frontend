12ab37ef6e0ae7ecedbb244d0aa39b0d7ef9ab13
Trancever <dawidu@onet.pl>
2019-01-13 22:43:32 +0100

    feat: add ability to navigate from user to admin dashboard.

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -1,17 +1,29 @@
 /* @flow */
 import * as React from 'react';
+import { connect } from 'react-redux';
 import { makeStyles } from '@material-ui/styles';
+import Button from '@material-ui/core/Button';
+import NavigationIcon from '@material-ui/icons/Navigation';
+import { navigate } from '@reach/router';
 
 import ElectionsSidebar from './ElectionsSidebar';
 import Appbar from './Appbar';
 
+import { type State } from '../redux/types/state';
+import { type User } from '../types';
+
 type Props = {
   children: React.Node,
   logout: () => mixed,
+  user: ?User,
 };
 
 function UserDashboard(props: Props) {
   const classes = useStyles();
+
+  function navigateToAdminPanel() {
+    navigate('/dashboard/admin');
+  }
 
   return (
     <div>
@@ -22,6 +34,18 @@ function UserDashboard(props: Props) {
         </div>
         <div className={classes.container}>{props.children}</div>
       </div>
+      {props.user && props.user.isAdmin ? (
+        <Button
+          variant="extendedFab"
+          color="primary"
+          aria-label="Navigate to admin panel"
+          className={classes.fab}
+          onClick={navigateToAdminPanel}
+        >
+          <NavigationIcon className={classes.extendedIcon} />
+          Panel admina
+        </Button>
+      ) : null}
     </div>
   );
 }
@@ -40,6 +64,16 @@ const useStyles = makeStyles({
   container: {
     flex: 4,
   },
+  fab: {
+    position: 'fixed',
+    right: '0',
+    bottom: '0',
+    margin: '20px',
+  },
 });
 
-export default UserDashboard;
+const mapStateToProps = ({ user }: State) => ({
+  user: user.user,
+});
+
+export default connect(mapStateToProps)(UserDashboard);
